c8079fcb56adf1daa935fc04932211a2919dcfa8
Philip Meulengracht <phailmollen@gmail.com>
2019-02-07 11:27:33 +0100

    fixes to existing directories

--- build-clang.sh
+++ build-clang.sh
@@ -8,6 +8,8 @@ if ! [ -x "$(command -v clang)" ]; then
     mkdir -p build
     cd build
     cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_EH=True -DLLVM_ENABLE_RTTI=True ../../sources/llvm
+  else
+  cd build
   fi
   make
   sudo make install
