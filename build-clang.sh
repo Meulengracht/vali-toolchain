917f80b86682845b3cb1e06cf4df25c9b40d32e8
Philip Meulengracht <phailmollen@gmail.com>
2019-03-26 13:29:41 +0000

    Make sure we can upgrade clang

--- build-clang.sh
+++ build-clang.sh
@@ -3,15 +3,13 @@
 mkdir -p output
 cd output
 
-if ! [ -x "$(command -v clang)" ]; then
-  if [ ! -d ./build ]; then
-    mkdir -p build
-    cd build
-    cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_EH=True -DLLVM_ENABLE_RTTI=True ../../sources/llvm
-  else
+if [ ! -d ./build ]; then
+  mkdir -p build
   cd build
-  fi
-  make
-  sudo make install
-  cd ..
+  cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_EH=True -DLLVM_ENABLE_RTTI=True ../../sources/llvm
+else
+  cd build
 fi
+make
+sudo make install
+cd ..
