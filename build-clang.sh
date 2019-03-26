28314a70440e3158521ec01dce4107eaedabfad7
Philip Meulengracht <phailmollen@gmail.com>
2019-03-26 13:38:25 +0000

    Updated scripts to execute correctly on this linux machine too

--- build-clang.sh
+++ build-clang.sh
@@ -7,9 +7,14 @@ if [ ! -d ./build ]; then
   mkdir -p build
   cd build
   cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_EH=True -DLLVM_ENABLE_RTTI=True ../../sources/llvm
+  make
+  sudo make install
+  cd ..
+  cd ..
 else
   cd build
+  make
+  sudo make install
+  cd ..
+  cd ..
 fi
-make
-sudo make install
-cd ..
