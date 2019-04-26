4d725f08c125ec2ac4ec2c2c65d54d2540857d47
Philip Meulengracht <phailmollen@gmail.com>
2019-04-26 12:49:13 +0200

    Remove unneccessary compile flags as we should let the system compiler compilet the toolchain

--- build-cross.sh
+++ build-cross.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 mkdir -p output
-cd output
+cd output 
 
 if [ ! -d ./cross-build ]; then
   mkdir -p cross-build
   cd cross-build
-  cmake -G "Unix Makefiles" -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=True -DLLVM_ENABLE_EH=True -DLLVM_ENABLE_RTTI=True -DCLANG_DEFAULT_RTLIB=compiler-rt -DCLANG_DEFAULT_CXX_STDLIB=libc++ -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_TESTS=Off -DLLVM_INCLUDE_EXAMPLES=Off -DCMAKE_INSTALL_PREFIX=$CROSS -DLLVM_DEFAULT_TARGET_TRIPLE=i386-pc-win32-itanium-coff -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_USE_LINKER=lld ../../sources/llvm
+  cmake -G "Unix Makefiles" -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=True -DLLVM_ENABLE_EH=True -DLLVM_ENABLE_RTTI=True -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_TESTS=Off -DLLVM_INCLUDE_EXAMPLES=Off -DCMAKE_INSTALL_PREFIX=$CROSS -DLLVM_DEFAULT_TARGET_TRIPLE=i386-pc-win32-itanium-coff ../../sources/llvm
   make
   sudo make install
   cd ../..
