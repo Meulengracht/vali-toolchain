d5f6e9d914e245afcbfd4374b113f190e3c904ca
Philip Meulengracht <phailmollen@gmail.com>
2020-06-08 09:30:12 +0200

    fix some more things

--- depends.sh
+++ depends.sh
@@ -5,12 +5,12 @@ cd sources
 
 # Dev-libraries
 sudo apt-get -qq install zip libelf1 libelf-dev libffi7 libffi-dev make gcc g++ git flex bison python libyaml-dev
-sudo python get-pip.py
+sudo python .\get-pip.py
 pip install prettytable Mako pyaml dateutils --upgrade
 
 # cmake version 3 is required
 CMAKE_VERSION="$(cmake --version)";
-if [[ $CMAKE_VERSION != *"3.13.0"* ]]; then
+if [[ $CMAKE_VERSION != *"3."* ]]; then
   if [ ! -f ./cmake-3.13.0-rc3 ]; then
     wget https://cmake.org/files/v3.13/cmake-3.13.0-rc3.tar.gz
     tar xzf cmake-3.13.0-rc3.tar.gz
