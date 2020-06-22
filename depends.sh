10d00bd420e7ea43a24d182cd67b1ede127ef3d0
Philip Meulengracht <phailmollen@gmail.com>
2020-06-22 10:18:46 +0200

    Update depends.sh

--- depends.sh
+++ depends.sh
@@ -4,7 +4,9 @@ mkdir -p sources
 cd sources
 
 # Dev-libraries
-sudo apt-get install cmake zip libelf1 libelf-dev libffi6 libffi7 libffi-dev make gcc g++ git flex bison python libyaml-dev
+sudo apt-get install cmake zip libelf1 libelf-dev libffi-dev make gcc g++ git flex bison python libyaml-dev
+sudo apt-get install libffi6
+sudo apt-get install libffi7
 sudo python ../get-pip.py
 pip install prettytable Mako pyaml dateutils --upgrade
 
