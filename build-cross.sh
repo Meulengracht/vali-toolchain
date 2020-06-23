1d188abedfa5f86a1e4cb80783d5cc00d980aa5b
Philip Meulengracht <phailmollen@gmail.com>
2020-06-23 17:43:15 +0200

    Update build-cross.sh

--- build-cross.sh
+++ build-cross.sh
@@ -7,7 +7,9 @@ if [ ! -d ./cross-build ]; then
   mkdir -p cross-build
   cd cross-build
   cmake -G "Unix Makefiles" -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=True -DLLVM_ENABLE_EH=True -DLLVM_ENABLE_RTTI=True -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_TESTS=Off -DLLVM_INCLUDE_EXAMPLES=Off -DCMAKE_INSTALL_PREFIX=$CROSS -DLLVM_DEFAULT_TARGET_TRIPLE=i386-pc-win32-itanium-coff ../../sources/llvm
-  make
+  
+	$(eval CPU_COUNT = $(shell nproc))
+  make -j$(CPU_COUNT)
   sudo make install
   cd ../..
 else
