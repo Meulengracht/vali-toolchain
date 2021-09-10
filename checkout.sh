fb3cb6643426c6e98dd5977dd8f84ac31bb01815
Philip Meulengracht <phailmollen@gmail.com>
2021-09-10 15:13:16 +0200

    Update checkout.sh

--- checkout.sh
+++ checkout.sh
@@ -3,7 +3,7 @@
 mkdir -p sources
 cd sources
   
-if [ ! -d ./llvm ]; then
+if [ ! -d ./llvm-project ]; then
   echo Checking out llvm
   git clone https://github.com/umbrella-c/llvm-project
   echo Checkout done
