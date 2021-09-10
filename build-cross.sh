ed07a021601633555b8caebd5ff3c6bbbe5b7c1d
Philip Meulengracht <phailmollen@gmail.com>
2021-09-10 15:13:35 +0200

    Update build-cross.sh

--- build-cross.sh
+++ build-cross.sh
@@ -6,7 +6,7 @@ cd output
 if [ ! -d ./cross-build ]; then
   mkdir -p cross-build
   cd cross-build
-  cmake -G "Unix Makefiles" -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=True -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;libcxx;libcxxabi;libunwind;lldb;compiler-rt;lld' -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_TESTS=Off -DLLVM_INCLUDE_EXAMPLES=Off -DCMAKE_INSTALL_PREFIX=$CROSS -DLLVM_DEFAULT_TARGET_TRIPLE=amd64-uml-vali ../../sources/llvm-project
+  cmake -G "Unix Makefiles" -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=True -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;libcxx;libcxxabi;libunwind;lldb;compiler-rt;lld' -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_TESTS=Off -DLLVM_INCLUDE_EXAMPLES=Off -DCMAKE_INSTALL_PREFIX=$CROSS -DLLVM_DEFAULT_TARGET_TRIPLE=amd64-uml-vali ../../sources/llvm-project/llvm
   
   #$(eval CPU_COUNT = $(shell nproc))
   #make -j$(CPU_COUNT)
