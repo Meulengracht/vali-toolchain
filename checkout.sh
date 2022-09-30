f98caf2239addcf7f5e0ec70953b05322ea4b471
Philip Meulengracht <the_meulengracht@hotmail.com>
2022-09-30 10:05:55 +0200

    checkout.sh: don't use latest commit yet

--- checkout.sh
+++ checkout.sh
@@ -6,6 +6,9 @@ cd sources
 if [ ! -d ./llvm-project ]; then
   echo Checking out llvm
   git clone https://github.com/umbrella-c/llvm-project
+  cd llvm-project
+  git checkout 108db006bde2fbe31fae792ed9a451c5d894cf14
+  cd ..
   echo Checkout done
 fi
 
