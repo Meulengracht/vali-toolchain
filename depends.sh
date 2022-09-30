2c32a9258226aedaa265fd1407711614c5ac3fc0
Philip Meulengracht <the_meulengracht@hotmail.com>
2022-09-30 10:32:38 +0200

    depends.sh: use only python3

--- depends.sh
+++ depends.sh
@@ -4,11 +4,10 @@ mkdir -p sources
 cd sources
 
 # Dev-libraries
-sudo apt-get install cmake zip libelf1 libelf-dev libffi-dev make gcc g++ git flex bison python python3 libyaml-dev
+sudo apt-get install cmake zip libelf1 libelf-dev libffi-dev make gcc g++ git flex bison python3 python3-venv python3-pip libyaml-dev
 sudo apt-get install libffi6
 sudo apt-get install libffi7
-sudo python ../get-pip.py
-pip install prettytable Mako pyaml dateutils --upgrade
+pip3 install prettytable Mako pyaml dateutils --upgrade
 
 # Leave sources
 cd ..
